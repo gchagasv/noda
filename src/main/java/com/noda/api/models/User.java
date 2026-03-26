cd8aee6566b14130cb687418c23795a745d101a2
Gabriel Chagas <chagasgabriel.gcv@gmail.com>
2026-03-26 20:49:50 -0300

    featfeat: implement User entity with JPA annotations

--- src/main/java/com/noda/api/models/User.java
+++ src/main/java/com/noda/api/models/User.java
@@ -1,5 +1,6 @@
 package com.noda.api.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
+    private String password;
 }
