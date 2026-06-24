e78da4c57c62028f6d33e90afda876e6a6a2d301
Gabriel Chagas <chagasgabriel.gcv@gmail.com>
2026-06-24 18:36:16 -0300

    feat: implement UserDetails on User entity and add BCrypt, JWT and security config

--- src/main/java/com/noda/api/models/User.java
+++ src/main/java/com/noda/api/models/User.java
@@ -5,8 +5,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -15,7 +19,7 @@ import java.util.List;
 @Table(name = "tb_user")
 @NoArgsConstructor
 @AllArgsConstructor
-public class  User {
+public class  User  implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,4 +48,37 @@ public class  User {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Address address;
+
+
+    // User Details interface
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_CLIENT"));
+    }
+
+    @Override
+    public  boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
